f4f5504c2b2c5a6dc0ba186190fe3e9011545920
Jenn Franco <jfranco376@gmail.com>
2022-08-06 01:41:43 -0500

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import AddressBook from './Components/AddressBook'
 
-
-  
 export default function App() {
 
   return (
     <div>
       <AddressBook/>
+
     </div>
   )
   }
- 
+
+
--- src/Components/AddressBook.jsx
+++ src/Components/AddressBook.jsx
@@ -1,21 +1,52 @@
-// import React, {useState} from "react";
-import React, { useEffect } from 'react'
+// // import React, { useEffect } from 'react'
 
+// // export default function AddressBook() {
 
-export default function AddressBook() {
+// //   useEffect(() => {
+// //     fetch('https://randomuser.me/api?results=25')
+// //     .then(response => response.json())
+// //     .then(json => console.log(json))
+// //     }, []);
 
-  useEffect(() => {
-    fetch('https://randomuser.me/api?results=25')
-    .then(response => response.json())
-    .then(json => console.log(json))
-    }, []);
+// //   return (
+// //   <div>
+// //     <h2><strong>ADDRESS BOOK USERS</strong></h2>
+// //   </div>
+// //   )
+// // }
 
+import userEvent from "@testing-library/user-event";
+import React, { Component } from "react";
 
-  return (
-  <div>
-    <h2><strong>ADDRESS BOOK USERS</strong></h2>
-  </div>
-  )
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      arrayOfUsers: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://randomuser.me/api?results=25")
+      .then((response) => {
+        return response.json();
+      })
+      .then((arrayOfUsers) => this.setState({ arrayOfUsers }));
+  }
+
+  render() {
+    return (
+      <div className="AddressBook">
+        <h2>
+          <strong>ADDRESS BOOK USERS</strong>
+        </h2>
+        {/* <ol>
+          {this.state.arrayOfUsers.map(() => {
+            return <li></li>;
+          })}
+        </ol> */}
+      </div>
+    );
+  }
 }
-
-
